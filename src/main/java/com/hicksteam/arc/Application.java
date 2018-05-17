5c17b6782126e0f3e9ac3d1c86ef7da5a9d8bef6
shicks <shicks255@yahoo.com>
2018-05-17 11:50:23 -0400

    adding stuff to initalize comments

--- src/main/java/com/hicksteam/arc/Application.java
+++ src/main/java/com/hicksteam/arc/Application.java
@@ -43,18 +43,32 @@ public class Application
             jdbcTemplate.execute("DROP TABLE IF EXISTS posts");
             jdbcTemplate.execute("CREATE TABLE posts(" +
                     "id SERIAL, title VARCHAR(255), content VARCHAR(255), author_id bigint)");
+            log.info("created table POSTS");
+
+            jdbcTemplate.execute("DROP TABLE IF EXISTS comments");
+            jdbcTemplate.execute("CREATE TABLE comments(" +
+                    "id SERIAL, author_id bigint, parent_comment_id bigint, post_id bigint, content VARCHAR(255))");
+            log.info("created table COMMENTS");
 
             // Split up the array of whole names into an array of first/last names
-            List<Object[]> splitUpData = new ArrayList<>();
-            splitUpData.add(new Object[] {"title1", "MyContent", 1});
-            splitUpData.add(new Object[] {"title2", "MyContent2", 2});
-            splitUpData.add(new Object[] {"testTitle", "MyContent3", 3});
+            List<Object[]> postData = new ArrayList<>();
+            postData.add(new Object[] {"title1", "MyContent", 1});
+            postData.add(new Object[] {"title2", "MyContent2", 2});
+            postData.add(new Object[] {"testTitle", "MyContent3", 3});
 
             // Use a Java 8 stream to print out each tuple of the list
-            splitUpData.forEach(data -> log.info(String.format("Inserting post record for %s %s %s", data[0], data[1], data[2])));
+            postData.forEach(data -> log.info(String.format("Inserting post record for %s %s %s", data[0], data[1], data[2])));
 
             // Uses JdbcTemplate's batchUpdate operation to bulk load data
-            jdbcTemplate.batchUpdate("INSERT INTO posts(title, content, author_id) VALUES (?,?,?)", splitUpData);
+            jdbcTemplate.batchUpdate("INSERT INTO posts(title, content, author_id) VALUES (?,?,?)", postData);
+
+
+            List<Object[]> commentData = new ArrayList<>();
+            commentData.add(new Object[] {1, 1, 0, "Comment 1 for post 1"});
+            commentData.add(new Object[] {2, 1, 1, "Comment 2 for post 1, child of comment 1"});
+            commentData.add(new Object[] {3, 2, 0, "Comment 1 for post 2"});
+            commentData.forEach(data -> log.info(String.format("Inserting comment record for id-%s postId-%s commentParrent-%s content-%s", data[0], data[1], data[2], data[3])));
+            jdbcTemplate.batchUpdate("INSERT INTO comments(id, post_id, parent_Comment_id, content) VALUES (?,?,?,?)", commentData);
 
             log.info("Querying for post records where title = 'testTitle':");
             String query = "SELECT id, title, content, author_id FROM posts WHERE title = ?";
