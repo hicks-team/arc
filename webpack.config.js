bb4d9830f895f5d9780b09bc194cb580dd9214ba
Eric Hicks <ehicks05@yahoo.com>
2018-05-18 11:44:15 -0400

    initial commit of react skeleton.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+var Visualizer = require('webpack-visualizer-plugin');
+
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+module.exports = {
+    entry: './src/main/webapp/js/dev/index.js',
+    output: {
+        path: path.resolve('src/main/webapp/js/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            { test: /\.(png|jpg|gif|svg|eot|woff2|woff|ttf)/,
+                use: [
+                    { loader: "url-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }, {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new Visualizer(), new HardSourceWebpackPlugin()
+    ]
+}
