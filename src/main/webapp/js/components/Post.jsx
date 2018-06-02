cd56c27299efdbea3fe9ef6279e61e8109be6f63
shicks <shicks255@yahoo.com>
2018-06-02 15:56:19 -0400

    rough reply mechanism inplace for each comment

--- src/main/webapp/js/components/Post.jsx
+++ src/main/webapp/js/components/Post.jsx
@@ -39,22 +39,66 @@ export default class Post extends React.Component {
     }
 }
 
-const Comments = ({commentTree}) => (
-    <div style={{padding: "2px 2px 2px 10px"}}>
-        {
-            commentTree.map((comment, i) => {
-                return (
-                    <div className="box is-small" style={{padding: "2px 2px 2px 10px"}} key={comment.id}>
-                        {comment.id}
+
+class Comments extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state= {
+                commentTree: props.commentTree,
+                replyBox: false,
+            }
+    }
+
+    render()
+    {
+        const box = this.state.commentTree.map((comment, i) => {
+
+            const replyBox = this.state.replyBox && this.state.replyNumber === i ?
+                <div>
+                <textarea defaultValue='Enter reply here'>
+                </textarea>
+                    <br/>
+                    <button onClick={() => console.log('hi')}>
+                        Submit
+                    </button>
+                </div> :
+                '';
+
+            return (
+                <div className="box is-small" style={{padding: "2px 2px 2px 10px"}} key={comment.id}>
+                    <span>
                         <br/>
                         {comment.author}
                         <br />
-                        {comment.content}
+                        {comment.id}-{comment.content}
+                        <br/>
+                    </span>
+                    <a onClick={() => this.showReplyBox(i)}>Reply</a>
 
-                        <Comments commentTree={comment.children} />
-                    </div>
-                );
-            })
-        }
-    </div>
-);
+                    {replyBox}
+
+                    <Comments commentTree={comment.children} />
+                </div>
+            );
+        })
+
+
+        return (
+            <div style={{padding: "2px 2px 2px 10px"}}>
+                {box}
+            </div>
+        );
+    }
+
+    showReplyBox(number)
+    {
+        this.setState({
+            replyBox: !this.state.replyBox,
+            replyNumber: number,
+        })
+
+    }
+
+}
